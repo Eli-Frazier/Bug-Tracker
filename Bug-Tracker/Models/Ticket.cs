797accf0e1e122dd4b6bca0bf7204fa46b86a8bc
Eli-Frazier <42358273+Eli-Frazier@users.noreply.github.com>
2018-09-25 16:57:24 -0400

    Add project files.

---
+++ Bug-Tracker/Models/Ticket.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bug_Tracker.Models
+{
+    public class Ticket
+    {
+        public int Id { get;set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset? Updated { get; set; }
+
+        public int ProjectId { get; set; }
+        public int TicketTypeId { get; set; }
+        public int TicketPriorityId { get; set; }
+        public int TicketStatusId { get; set; }
+        public string OwnerUserId { get; set; }
+        public string AssignedToUserId { get; set; }
+
+        //Children
+        public virtual ICollection<TicketAttachment> TicketAttachments { get; set; }
+        public virtual ICollection<TicketComment> TicketComments { get; set; }
+        public virtual ICollection<TicketHistory> TicketHistories { get; set; }
+        public virtual ICollection<TicketNotification> TicketNotifications { get; set; }
+
+        //Parents
+        public virtual Project Project { get; set; }
+        public virtual TicketType TicketType { get; set; }
+        public virtual TicketPriority TicketPriority { get; set; }
+        public virtual TicketStatus TicketStatus { get; set; }
+        public virtual ApplicationUser OwnerUser { get; set; }
+        public virtual ApplicationUser AssignedToUser { get; set; }
+
+        public Ticket()
+        {
+            TicketNotifications = new HashSet<TicketNotification>();
+            TicketHistories = new HashSet<TicketHistory>();
+            TicketComments = new HashSet<TicketComment>();
+            TicketAttachments = new HashSet<TicketAttachment>();
+
+        }
+
+    }
+}
